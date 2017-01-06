b011a9260f6327d3bf88779add28237675deface
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-06 15:31:43 +0100

    Initial commit

---
+++ src/Country.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Михаил on 06.01.2017.
+ */
+public class Country {
+
+    private String name;
+    private double area;
+    private int population;
+
+    public Country(String n, double a, int pop) {
+        name = n;
+        area = a;
+        population = pop;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
---
+++ src/CountrySet.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Михаил on 06.01.2017.
+ */
+public class CountrySet {
+
+    ArrayList<Country> countries;
+
+    public CountrySet() {
+        countries = new ArrayList<>();
+    }
+
+    public void addCountry(Country c) {
+        countries.add(c);
+    }
+
+    public void LargestPopulation() {
+
+    }
+
+
+}
