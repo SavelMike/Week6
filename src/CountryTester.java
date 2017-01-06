246c80313c00962f59f2dcf9d275cf3b7e8ca95f
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-06 17:46:27 +0100

    Program Country

--- src/CountryTester.java
+++ src/CountryTester.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+import java.io.File;
+
 /**
  * Created by Михаил on 06.01.2017.
  */
 public class CountryTester {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input file name: ");
+        String filename = sc.next();
+        File f = new File(filename);
+        Scanner scanner = new Scanner(f);
+        while (scanner.hasNextLine()) {
+            String line = scanner.next();
+            Scanner scStr = new Scanner(line);
+            if (scStr.hasNext()) {
+                country = scStr.next();
+            } else {
+                System.exit(1);
+            }
+            if (scStr.hasNextDouble()) {
+                area = scStr.nextDouble();
+            } else {
+                System.exit(1);
+            }
+            if (scStr.hasNextInt()) {
+                population = scStr.nextInt();
+            } else {
+                System.exit(1);
+            }
+            Country c = new Country(country, area, population);
+            sc.addCount;
 
-        Country c = new Country("Jamaica", 23455, 9876);
-        c.getPopulation();
-        c.add
+        }
 
     }
 }
