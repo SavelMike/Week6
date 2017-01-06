be386a132fcf44ec07525b945c7a1f38ae99d6f4
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-06 16:25:42 +0100

    Program Country

--- src/CountrySet.java
+++ src/CountrySet.java
@@ -15,9 +15,33 @@ public class CountrySet {
         countries.add(c);
     }
 
-    public void LargestPopulation() {
-
+    public Country LargestPopulation() {
+        Country currentPop = countries.get(0);
+        for (int i = 0; i < countries.size(); i++) {
+            if (currentPop.getPopulation() < countries.get(i).getPopulation()) {
+                currentPop = countries.get(i);
+            }
+        }
+        return currentPop;
     }
 
+    public Country LargestArea() {
+        Country currentAr = countries.get(0);
+        for (int i = 0; i < countries.size(); i++) {
+            if (currentAr.getArea() < countries.get(i).getArea()) {
+                currentAr = countries.get(i);
+            }
+        }
+        return currentAr;
+    }
 
+    public Country LargestPopDensity() {
+        Country currentDen = countries.get(0);
+        for  (int i = 0; i < countries.size(); i++) {
+            if (currentDen.getPopulation()/currentDen.getArea() < countries.get(i).getPopulation()/countries.get(i).getArea()) {
+                currentDen = countries.get(i);
+            }
+        }
+        return currentDen;
+    }
 }
---
+++ src/CountryTester.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Михаил on 06.01.2017.
+ */
+public class CountryTester {
+    public static void main(String[] args) {
+
+        Country c = new Country("Jamaica", 23455, 9876);
+        c.getPopulation();
+        c.add
+
+    }
+}
